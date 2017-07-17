8c1cce714e7a4c8f915d685878a9a2dfad4b7bbb
Isai <isai.salas.gonzalez@gmail.com>
2017-07-16 21:05:10 -0400

    Add license

--- phyla_amphora_benchmark/compareuclust2phylaamphora.pl
+++ phyla_amphora_benchmark/compareuclust2phylaamphora.pl
@@ -1,4 +1,24 @@
+
 #!/usr/bin/perl
+
+# (C) Copyright 2017 Isai Salas Gonzalez
+#
+#    This file is part of gfobap.
+#
+#    gfobap is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    gfobap is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with gfobap.  If not, see <http://www.gnu.org/licenses/>.
+
+
 use strict;
 use warnings;
 use Data::Dumper;
